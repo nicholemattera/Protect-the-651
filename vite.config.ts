e4f24094551ba817de2aed6a1262bee2784dabf9
Nickel Mattera <me@nicholemattera.gay>
2026-03-17 20:58:15 -0500

    chore: add linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,16 @@
 import { defineConfig } from 'vite'
 import { globSync } from 'node:fs'
-import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: {
-        ...globSync('*.html').reduce<Record<string, string>>((input, file) => {
-          input[file.substring(0, file.length - 5)] = file
-          return input
-        }, {}),
-      },
+      input: globSync('*.html').reduce<Record<string, string>>((input, file) => {
+        input[file.substring(0, file.length - 5)] = file
+        return input
+      }, {}),
     },
   },
   plugins: [tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
